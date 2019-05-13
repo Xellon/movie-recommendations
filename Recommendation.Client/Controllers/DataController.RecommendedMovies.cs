6a62ea4e46673f95ec492af7b8006119b2b2e10a
Xellon <eseviakov@gmail.com>
2019-05-13 11:49:02 +0300

    Fixed recommended movie requests

--- Recommendation.Client/Controllers/DataController.RecommendedMovies.cs
+++ Recommendation.Client/Controllers/DataController.RecommendedMovies.cs
@@ -1,42 +1,57 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
-using Recommendation.Database;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace Recommendation.Client.Controllers
 {
     public partial class DataController : ControllerBase
     {
         [HttpGet("recommendedmovies")]
-        public IEnumerable<RecommendedMovie> RecommendedMovies(string userId)
-        {
-            return _context.Recommendations.Where(r => r.UserId == userId)
+        public async Task<IEnumerable<RecommendedMovie>> RecommendedMovies(string userId) => 
+            await _context.Recommendations
+                .Where(r => r.UserId == userId)
                 .Join(_context.RecommendedMovies,
                     r => r.Id,
                     m => m.RecommendationId,
-                    (r, m) => m);
-        }
+                    (r, m) => m)
+                .Select(r => new RecommendedMovie { MovieId = r.MovieId, PossibleRating = (float)r.PossibleRating })
+                .ToListAsync();
 
         [HttpGet("recommendedmovies/{id}")]
-        public IEnumerable<RecommendedMovie> RecommendedMovies([FromRoute]int id, string userId)
+        public async Task<IEnumerable<RecommendedMovie>> RecommendedMovies([FromRoute]int id) =>
+            await _context.RecommendedMovies
+                .Where(m => m.RecommendationId == id)
+                .Select(r => new RecommendedMovie { MovieId = r.MovieId, PossibleRating = (float)r.PossibleRating })
+                .ToListAsync();
+
+        [HttpGet("recommendedmovies/{id}")]
+        public async Task<ActionResult<IEnumerable<RecommendedMovie>>> RecommendedMovies([FromRoute]int id, string userId)
         {
-            return _context.Recommendations.Where(r => r.UserId == userId && r.Id == id)
-                .Join(_context.RecommendedMovies,
-                    r => r.Id,
-                    m => m.RecommendationId,
-                    (r, m) => m);
+            var recommendation = await _context.Recommendations.FirstOrDefaultAsync(r => r.Id == id && r.UserId == userId);
+
+            if (recommendation is null)
+                return BadRequest();
+
+            return Ok(await RecommendedMovies(id));
         }
 
         [HttpGet("recommendedmovies/latest")]
-        public IEnumerable<RecommendedMovie> LatestRecommendedMovies(string userId)
+        public async Task<ActionResult<IEnumerable<RecommendedMovie>>> LatestRecommendedMovies(string userId)
         {
-            // TODO: change max id to max date
-            var recommendations = _context.Recommendations.Where(r => r.UserId == userId);
+            var recommendation = await _context.Recommendations.OrderByDescending(r => r.Date).FirstOrDefaultAsync(r => r.UserId == userId);
 
-            if (!recommendations.Any())
-                return new RecommendedMovie[0];
+            if (recommendation is null)
+                return BadRequest();
 
-            return _context.RecommendedMovies.Where(m => m.RecommendationId == recommendations.Max(r => r.Id));
+            return Ok(await RecommendedMovies(recommendation.Id));
         }
+    }
+ 
+    public struct RecommendedMovie
+    {
+        public int MovieId { get; set; }
+        public float PossibleRating { get; set; }
     }
 }
